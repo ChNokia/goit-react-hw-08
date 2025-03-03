1661f1f61608f77f7871595f09132cbf4a56e5c3
ChNokia <chizarus@gmail.com>
2025-03-03 13:58:36 +0200

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,27 @@
 import { TailSpin } from "react-loader-spinner";
+import { RotatingLines } from 'react-loader-spinner';
 
-const Loader = () => {
-  return <TailSpin radius={"8px"} height="60" width="60" />;
+import css from './Loader.module.css';
+
+//const Loader = () => {
+ // return <TailSpin radius={"8px"} height="60" width="60" />;
+//};
+const Loader = ({ isLoading, strokeColor = '#000000' }) => {
+  return (
+    <div className={css.loader}>
+      <RotatingLines
+        visible={isLoading}
+        height="100%"
+        width="100%"
+        strokeColor={strokeColor}
+        strokeWidth="5"
+        animationDuration="0.75"
+        ariaLabel="rotating-lines-loading"
+        // wrapperStyle={{}}
+        // wrapperClass=""
+      />
+    </div>
+  );
 };
 
 export default Loader;
